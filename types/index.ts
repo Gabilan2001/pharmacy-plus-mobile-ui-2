d67e204a7e5a52fd8800a1bd7c7fec97cad02d8e
sewmi <sewmisenarath@gmail.com>
2025-10-23 01:33:27 +0530

    added discount feature for pharmacy medicine

--- types/index.ts
+++ types/index.ts
@@ -41,6 +41,9 @@ export type Medicine = {
   expiryDate?: string;
   isNearExpiry?: boolean;
   isExpired?: boolean;
+  isDiscounted?: boolean;
+  discountedPrice?: number;
+  discountPercentage?: number;
   _id?: string;
 };
 
